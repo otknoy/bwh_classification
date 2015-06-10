4f21a42d6db15697b3e8a623308799cbd48b06aa
otknoy <otknoy@gmail.com>
2015-06-10 21:27:37 +0900

    Visualize using matplotlib

--- clasify.py
+++ clasify.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from sklearn.cluster import KMeans
-import numpy as np
 
 def kmeans(features, k=8):
     km = KMeans(n_clusters=k, n_jobs=4)
@@ -11,13 +10,27 @@ def kmeans(features, k=8):
 if __name__ == '__main__':
     from bwh import load_bwh_data
     names, features = load_bwh_data()
-    features = np.array(features)
+
 
     labels = kmeans(features, k=10)
 
-    print "label, name, b, w, h"
-    for l, n, f in zip(labels, names, features):
-        b, w, h = f
+    from mpl_toolkits.mplot3d import Axes3D
+    import matplotlib.pyplot as plt
+    import numpy as np
 
-        print "%d,%s,%d,%d,%d" % (l, n.encode('utf-8'), b, w, h)
+    features = np.array(features)
+    x, y, z = features.T
 
+    print x, y, z
+
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.plot(x, y, z)
+
+    plt.show()
+
+    # print "label, name, b, w, h"
+    # for l, n, f in zip(labels, names, features):
+    #     b, w, h = f
+
+    #     print "%d,%s,%d,%d,%d" % (l, n.encode('utf-8'), b, w, h)
